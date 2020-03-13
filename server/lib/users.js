300946e8d0c2398f757af497bb214bd9dbd06a78
Daniela <victrola@abv.bg>
2020-03-13 21:34:09 +0000

    Add session saving logic and authentication validation

--- server/lib/users.js
+++ server/lib/users.js
@@ -2,6 +2,15 @@ const bcrypt = require("bcrypt");
 // const validate = require("validate"); // validate is a great module to validate data: https://validatejs.org/
 const sql = require("./db/config.js");
 
+function saveSession(session, passKey) {
+  session.key = passKey;
+}
+
+function removeSensitive(data) {
+  delete data.password;
+  return data;
+}
+
 const users = {
   async register(data) {
     const { username, password, confirmPassword, email, confirmEmail } = data;
@@ -18,16 +27,20 @@ const users = {
       console.log(e);
     }
   },
-  async authenticate(userName, password) {
+  async authenticate(session, data) {
+    const { username, password } = data;
     let rows = await sql("tbl_users")
       .select()
-      .whereRaw("username = ?", [userName]);
-    const match = await bcrypt.compare(password, rows[0].password);
-    if (match) {
-      return true;
-    } else {
-      return false;
+      .whereRaw("username = ?", [username]);
+    if (rows.length) {
+      const match = await bcrypt.compare(password, rows[0].password);
+      if (match) {
+        saveSession(session, rows[0].password);
+        return removeSensitive(rows[0]);
+      }
     }
+    // If this line is reached that means the either the username or the password is invalid
+    throw { _general: "Invalid username or password" };
   },
   async session(key) {
     if (key) {
@@ -36,9 +49,10 @@ const users = {
         .whereRaw("password = ?", [key]);
       // If the session key matches a password key from the database then authenticate the user
       if (rows.length) {
-        return rows[0];
+        return removeSensitive(rows[0]);
       }
     }
+    // If this line is reached that means the session could not be loaded
     throw { _silent: "Session key could not be found or was empty" };
   },
   async deauthenticate() {
