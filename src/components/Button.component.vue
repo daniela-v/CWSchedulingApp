761447cf43f017c6ace339038ee9628407276f58
Cora <andreeacora98@gmail.com>
2020-03-14 16:50:06 +0000

    Add styling to buttons of type header

--- src/components/Button.component.vue
+++ src/components/Button.component.vue
@@ -1,11 +1,11 @@
 <template>
   <router-link :id="getName" class="button" :class="getStyle" v-if="href" :to="href">
-    <span class="text"><slot></slot></span>
     <Icon :name="icon"></Icon>
+    <span class="text"><slot></slot></span>
   </router-link>
   <div v-else :id="getName" class="button" :class="getStyle" @click="OnButtonClick">
-    <span class="text"><slot></slot></span>
     <Icon :name="icon"></Icon>
+    <span class="text"><slot></slot></span>
   </div>
 </template>
 
@@ -41,17 +41,33 @@ export default {
   display: inline-flex;
   align-items: center;
   cursor: pointer;
+  .icon {
+    margin: 0 8px 0 0; // Add spacing between icon and text on the right side of the icon [ICON  TEXT]
+  }
+}
+
+.header-button {
+  padding: 10px;
+  color: $color-light-grey;
+  font-size: 16px;
+  font-weight: 600;
+  transition: color .2s ease;
+  &:hover, &.active { color: lighten($color-light-grey, 20%); }
 }
 
 .menu-button {
   flex: 1;
   padding: 10px;
-  color: $color-beige;
   font-size: 24px;
   font-weight: 600;
+  border-radius: 4px;
   transition: color .2s ease, background-color .2s ease;
-  .icon { margin-left: 8px; }
-  &:hover, &.active { color: lighten($color-beige, 10%); }
+  .text { order: 1; }
+  .icon {
+    order: 2;
+    margin: 0 0 0 8px; // Add spacing between icon and text on the left side of the icon [TEXT  ICON]
+  }
+  &:hover, &.active { color: lighten($color-beige, 20%); }
   &.active { background-color: rgba($color-beige, .2); }
 }
 </style>
