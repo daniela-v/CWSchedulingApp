e25b09a0aed894fe1ed391b2d83c067e46409a3d
Daniela <victrola@abv.bg>
2020-03-14 20:06:14 +0000

    created login vue window

---
+++ src/components/Login.window.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login-overlay">
+    <div class="title">Log In</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import '@/scss/_colors';
+
+.login-overlay {
+  position: relative;
+  padding: 10px;
+  width: 380px;
+  .title {
+    margin: 20px 0;
+    color: $color-beige;
+    font-size: 40px;
+  }
+}
+</style>
