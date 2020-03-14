f037f8e295f5d3d9214ecdd6a509f149ce2b482f
Cora <andreeacora98@gmail.com>
2020-03-14 14:43:41 +0000

    Create basic grid layout for the header module

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <Header></Header>
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from './modules/Header.module.vue';
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap");
---
+++ src/modules/Header.module.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="logo"></div>
+    <section class="navigation"></section>
+    <section class="account"></section>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+header {
+  display: grid;
+  grid-template-columns: auto 1fr 200px;
+  grid-template-areas: "logo navigation account";
+
+  .logo {
+    grid-area: logo;
+  }
+  .navigation {
+    grid-area: navigation;
+  }
+  .account {
+    grid-area: account;
+  }
+}
+</style>
