c5af61ee7e7922ee534ade1f00af4ec0f576096b
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-02 14:36:57 +0100

    Added new seed data for new coursework table

---
+++ server/lib/db/seeds/coursework.js
@@ -0,0 +1,13 @@
+const rows = [
+  { title: 'admin_1', module: '%test%', inteneded_date: '10/06/2020', actual_completion_date: '21/06/2020', status: 'In progress' },
+  { title: 'admin_2', module: '?test?', inteneded_date: '21/12/2020', actual_completion_date: '22/08/2021', status: 'In progress' },
+  { title: 'admin_3', module: '.test`', inteneded_date: '03/06/2020', actual_completion_date: '12/03/2020', status: 'complete' },
+];
+
+async function insert(sql, tableName) {
+  await sql.table(tableName).del();
+  await sql.table(tableName).insert(rows);
+  console.log(`✓   "${tableName}" has been populated\n`);
+}
+
+module.exports = { insert };
