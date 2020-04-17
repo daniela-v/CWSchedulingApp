9f76d31a3abe0c87050ab9a3aade38a688073e5c
Andrew <andrew.wright.aw12@gmail.com>
2020-04-17 11:27:39 +0100

    Changes to coursework seed data

--- server/lib/db/seeds/coursework.js
+++ server/lib/db/seeds/coursework.js
@@ -1,7 +1,7 @@
 const rows = [
-  { title: 'admin_1', module: '%test%', intended_date: 1578268800000, completion_date: 1583625600000, status: 'In progress' },
-  { title: 'admin_2', module: '?test?', intended_date: 1578614400000, completion_date: 1585526400000, status: 'In progress' },
-  { title: 'admin_3', module: '.test`', intended_date: 1582416000000, completion_date: 1585526400000, status: 'Complete' },
+  { title: 'coursework_1', module: '%test%', intended_date: 1578268800000, completion_date: 1583625600000, status: 'In progress' },
+  { title: 'coursework_2', module: '?test?', intended_date: 1578614400000, completion_date: 1585526400000, status: 'In progress' },
+  { title: 'coursework_3', module: '.test`', intended_date: 1582416000000, completion_date: 1585526400000, status: 'Complete' },
 ];
 
 async function insert(model) {
