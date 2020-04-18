e248e2fb38c25cdc5b664715b9e463a8a525c540
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-18 21:13:57 +0100

    Added missing export module statement

--- server/lib/coursework.js
+++ server/lib/coursework.js
@@ -1,34 +1,35 @@
-const { Courseworks } = require('./db/models.js').models;
+const { Coursework } = require('./db/models.js').models;
 
 const courseworks = {
   async getCoursework(id) {
-    const courseworkFound = await Courseworks.findOne({ where: { id } });
+    const courseworkFound = await Coursework.findOne({ where: { id } });
     if (courseworkFound) {
       return courseworkFound;
     }
     throw { _notification: 'No coursework could be found with that title' };
   },
   async removeCoursework(id) {
-    await Courseworks.destroy({ where: { id } });
+    await Coursework.destroy({ where: { id } });
     throw { _notification: 'No coursework could be found with that title' };
   },
   async createCoursework(data) {
     const { owner, description, title, module, deleted, isPrivate, expectedDate, completedDate } = data;
-    Courseworks.create({ owner, description, title, module, deleted, isPrivate, expectedDate, completedDate });
+    Coursework.create({ owner, description, title, module, deleted, isPrivate, expectedDate, completedDate });
   },
   async changePrivacy(data) {
     const { id, setPrivacy } = data;
-    await Courseworks.update({ is_private: setPrivacy }, { where: { id } });
+    await Coursework.update({ is_private: setPrivacy }, { where: { id } });
   },
   async findAllPublic() {
-    const resultAll = await Courseworks.findAll({ where: { is_private: false } });
+    const resultAll = await Coursework.findAll({ where: { isPrivate: false } });
     return resultAll;
   },
   async findAllThatBelongToUser(owner) {
-    const resultAll = await Courseworks.findAll({ where: { owner } });
+    const resultAll = await Coursework.findAll({ where: { owner } });
     if (!resultAll) {
       throw { _notification: 'There are no courseworks that belong to that user' };
     }
     return resultAll;
   },
 };
+module.exports = courseworks;
