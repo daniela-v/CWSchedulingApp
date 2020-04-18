5d7ac18c6a781be9079c23b45f9304b1a6bd6c52
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-18 20:58:48 +0100

    Added fail state to find all courseworks that belong to a user

--- server/lib/coursework.js
+++ server/lib/coursework.js
@@ -1,31 +1,34 @@
-const { Coursework } = require('./db/models.js').models;
+const { Courseworks } = require('./db/models.js').models;
 
 const courseworks = {
   async getCoursework(id) {
-    const courseworkFound = await Coursework.findOne({ where: { id } });
+    const courseworkFound = await Courseworks.findOne({ where: { id } });
     if (courseworkFound) {
       return courseworkFound;
     }
     throw { _notification: 'No coursework could be found with that title' };
   },
   async removeCoursework(id) {
-    await Coursework.destroy({ where: { id } });
+    await Courseworks.destroy({ where: { id } });
     throw { _notification: 'No coursework could be found with that title' };
   },
   async createCoursework(data) {
     const { owner, description, title, module, deleted, isPrivate, expectedDate, completedDate } = data;
-    Coursework.create({ owner, description, title, module, deleted, isPrivate, expectedDate, completedDate });
+    Courseworks.create({ owner, description, title, module, deleted, isPrivate, expectedDate, completedDate });
   },
   async changePrivacy(data) {
     const { id, setPrivacy } = data;
-    await Coursework.update({ is_private: setPrivacy }, { where: { id } });
+    await Courseworks.update({ is_private: setPrivacy }, { where: { id } });
   },
   async findAllPublic() {
-    const resultAll = await Coursework.findAll({ where: { is_private: false } });
+    const resultAll = await Courseworks.findAll({ where: { is_private: false } });
     return resultAll;
   },
   async findAllThatBelongToUser(owner) {
-    const resultAll = await Coursework.findAll({ where: { owner } });
+    const resultAll = await Courseworks.findAll({ where: { owner } });
+    if (!resultAll) {
+      throw { _notification: 'There are no courseworks that belong to that user' };
+    }
     return resultAll;
   },
 };
