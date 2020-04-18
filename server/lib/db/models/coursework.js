163403c6e0b4a3c9b7ec23efe205257dd61e1ebb
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-18 17:15:16 +0100

    Updated the coursework model

--- server/lib/db/models/coursework.js
+++ server/lib/db/models/coursework.js
@@ -5,6 +5,14 @@ module.exports = (sequelize, type, table) => sequelize.define(`tbl_${table}`, {
     primaryKey: true,
     autoIncrement: true,
   },
+  owner: {
+    type: type.STRING(32),
+    allowNull: false,
+  },
+  description: {
+    type: type.STRING(500),
+    allowNull: true,
+  },
   title: {
     type: type.STRING(32),
     allowNull: false,
@@ -13,11 +21,27 @@ module.exports = (sequelize, type, table) => sequelize.define(`tbl_${table}`, {
     type: type.STRING(128),
     allowNull: false,
   },
-  intended_date: {
+  deleted: {
+    type: type.DATE,
+    allowNull: true,
+  },
+  private: {
+    type: type.Boolean,
+    allowNull: false,
+  },
+  createdAt: {
     type: type.DATE,
     allowNull: false,
   },
-  completion_date: type.DATE,
+  updatedAt: {
+    type: type.DATE,
+    allowNull: true,
+  },
+  expected_date: {
+    type: type.DATE,
+    allowNull: false,
+  },
+  completed_date: type.DATE,
   status: {
     type: type.STRING(16),
     allowNull: false,
