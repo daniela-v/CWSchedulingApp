0825303c978be48623557a5053023b312e0db915
awrigh206 <awrigh206@caledonian.ac.uk>
2020-04-19 13:26:56 +0100

    Added routing to get coursework by its id

--- server/routes/coursework.js
+++ server/routes/coursework.js
@@ -16,6 +16,19 @@ router.post('/new', async (req, res) => {
   }
   res.json({ result, error });
 });
+/**
+ * GET /coursework/get
+ */
+router.get('/get', async (req, res) => {
+  let error;
+  let result;
+  try {
+    result = await courseworks.getCoursework(req.query.id);
+  } catch (e) {
+    error = e;
+  }
+  res.json({ result, error });
+});
 
 /**
  * GET /coursework/publicList
